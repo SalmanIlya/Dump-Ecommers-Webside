001b904d1fb93b5ce6cd89ecce46d378dc552473
Salamanilyas <95139252+SalmanIlya@users.noreply.github.com>
2022-11-01 17:04:54 +0500

    add few more routes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,43 @@
 import React from 'react'
-import Home from './Component/Home'
+import Home from './Component/Home/Home'
 import { Route, Routes } from "react-router-dom"
-import Nave from './Component/Nave'
-import Cart from './Component/Cart'
-import PageNotFound from './Component/PageNotFound'
+import Nave from './Component/Navebar/Nave'
+import Cart from './Component/Cart/Cart'
+import PageNotFound from './Component/page not found/PageNotFound'
 import "./App.css"
-import ProductDetail from './Component/ProductDetail'
-import Footer from './Component/Footer'
-import Address from './Component/Address'
+import 'font-awesome/css/font-awesome.min.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+import ProductDetail from './Component/ProductDetail/ProductDetail'
+import Footer from './Component/footer/Footer'
+import Address from './Component/Address/Address'
 import Signin from './SignunpAndLogin/Signin'
 import Signup from './SignunpAndLogin/Signup'
+import About from './Component/about/About'
+import {ToastContainer} from "react-toastify"
+import Paymentscreen from './Component/Payment/Paymentscreen'
+import Product from './Component/products/MainPoroduct'
+import Contact from './Component/Contact/Contact'
 const App = () => {
   return (
     <div className='app'>
+      <ToastContainer/>
       <Nave />
-      <main className='main'>
+      <main className='main' >
 
         <Routes>
           <Route exect path="/" element={<Home />} />
+          <Route exect path="/contact" element={<Contact/>} />
+          <Route exect path="/product" element={<Product />} />
+          <Route exect path="/about" element={<About />} />
           <Route exect path="/product/:id" element={<ProductDetail />} />
           <Route exect path="/cart" element={<Cart />} />
           <Route exect path="/sign/in" element={<Signin />} />
           <Route exect path="/sign/up" element={<Signup />} />
           <Route exect path="/address" element={<Address />} />
-          <Route exect path="*" element={<PageNotFound />} />
+          <Route exect path="/cart/payment" element={<Paymentscreen />} />
+          <Route  path="*" element={<PageNotFound />} />
         </Routes>
-      </main>
+      </main> 
 
       <Footer />
     </div>
