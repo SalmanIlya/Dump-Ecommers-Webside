0e1072584a355bd45440498e39d14858f8ba7005
Salamanilyas <95139252+SalmanIlya@users.noreply.github.com>
2022-11-01 17:12:42 +0500

    add api of fake reviews

---
+++ src/Component/Home/Reviews.js
@@ -0,0 +1,24 @@
+const Reviews=[
+    {
+        id:1,
+        name:"	Nina",
+        image:"/user/1.jpg",
+        ratings:4.5,
+        text:"This switch was a great purchase! The ability to play handheld, on the dock, or in kickstand is pretty awesome. I like the oled screen it really has a nice video/picture quality to it. I also bought the hori split pad pro and the 8bitdo pro 2 controller with this and it’s a blast! I recommend the Oled Switch to any gamer of all ages"
+    },
+    {
+        id:2,
+        name:"	Juan D.",
+        image:"/user/2.jpg",
+        ratings:4,
+        text: "I say it's a MUST upgrade. The battery life and new joy cons alone are already worth upgrading for so the OLED display just seals it. I wish I upgraded sooner but it's never too late. My gripes were the loose dock, hard to remove microSD card and hard to open game cart cover but none of them were big deal. I just hope that they put a faster storage but maybe we'll see it on the next generation Switch."
+    },
+    {
+        id:3,
+        name:"Aamir Baig",
+        image:"/user/3.jpg",
+        ratings:5,
+        text:"Primeramente, el visor es de lo mejor qué hay en el mercado CONSIDERANDO el precio; un valve index es MUY caro, pero tiene adiciones qué el Quest 2 no y viceversa; eso es lo que lo hace un gran visor a comparación de los de tope de gama."
+    },
+]
+export default Reviews
