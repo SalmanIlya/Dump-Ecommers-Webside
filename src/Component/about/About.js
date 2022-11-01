1a3f0ad652b2149ffbc54fcbc62fa0a469d46e00
Salamanilyas <95139252+SalmanIlya@users.noreply.github.com>
2022-11-01 17:09:03 +0500

    create about page

---
+++ src/Component/about/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Helmet } from 'react-helmet-async'
+import "./style.css"
+
+const About = () => {
+  return (
+    <div className='card-main-div'>
+      <Helmet>
+                <title>
+                    home page
+                </title>
+            </Helmet>
+    </div>
+  )
+}
+
+export default About
