168127ab19f8de6a342291d440ee29a4a322b683
Salamanilyas <95139252+SalmanIlya@users.noreply.github.com>
2022-11-01 17:15:55 +0500

    create ratings

---
+++ src/Component/Ratings/Ratings.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const Ratings = ({ratings}) => {
+  return (
+    <div>
+      <span>
+ <i className={ratings>=1 ?'fas fa-star text-warning':
+ratings>=1.5 ? 'fas fa-star-half-alt text-warning':
+'far fa-star text-warning'
+}></i> 
+</span>
+<span>
+ <i className={ratings>=2 ?'fas fa-star text-warning':
+ratings>=1.5 ? 'fas fa-star-half-alt text-warning':
+'far fa-star text-warning'
+}></i> 
+</span>
+<span>
+ <i className={ratings>=3 ?'fas fa-star text-warning':
+ratings>=2.5 ? 'fas fa-star-half-alt text-warning':
+'far fa-star text-warning'
+}></i> 
+</span>
+<span>
+ <i className={ratings>=4 ?'fas fa-star text-warning':
+ratings>=3.5 ? 'fas fa-star-half-alt text-warning':
+'far fa-star text-warning'
+}></i> 
+</span>
+<span>
+ <i className={ratings===5 ?'fas fa-star text-warning':
+ratings>=4.5 ? 'fas fa-star-half-alt text-warning':
+'far fa-star text-warning'
+}></i> 
+</span>
+
+    </div>
+  )
+}
+
+export default Ratings
