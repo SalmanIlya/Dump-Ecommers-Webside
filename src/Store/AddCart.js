44f2777a46bcfd0b1e8220dd078d9ca7d34c6e80
Salamanilyas <95139252+SalmanIlya@users.noreply.github.com>
2022-11-08 19:01:31 +0500

    add-react-toestify

--- src/Store/AddCart.js
+++ src/Store/AddCart.js
@@ -1,5 +1,5 @@
 import { createSlice, current } from "@reduxjs/toolkit";
-// import { toast } from "react-toastify";
+import {  toast } from 'react-toastify';
 
 export const CartReducer = createSlice({
   name: "cartitem",
@@ -15,81 +15,77 @@ export const CartReducer = createSlice({
 
       if (finddata >= 0) {
         state.cart[finddata].CardQuantity += 1;
-        // toast.success("increase product CardQuantity in a cart", {
-        //   position: toast.POSITION.BOTTOM_LEFT,
-        // });
+        toast.success('increace product quantity', {
+          position: toast.POSITION.BOTTOM_LEFT
+      });
       } else {
         state.cart.push({ ...action.payload, CardQuantity: 1 });
-        // toast.success("product is add in a cart", {
-        //   position: toast.POSITION.BOTTOM_LEFT,
-        // });
+        toast.success('Add product in a card', {
+          position: toast.POSITION.BOTTOM_LEFT
+      });
       }
+      
     },
-
     removeItem: (state, action) => {
       state.cart = state.cart.filter((x) => x.id !== action.payload);
-      // toast.error("remove product from card", {
-      //   position: toast.POSITION.BOTTOM_LEFT,
-      // });
+      toast.error('Add product in a card', {
+        position: toast.POSITION.BOTTOM_LEFT
+    });
     },
     increase: (state, action) => {
       const currentState = current(state);
-      console.log(action.payload);
-      console.log(currentState);
       const index = currentState.cart.findIndex((item) => {
-        console.log("item", item);
-        console.log("action.payload.id", action.payload);
         return item.id === action.payload;
       });
-      
-
-      
       if (state.cart[index].CardQuantity) {
         state.cart[index].CardQuantity++;
-        console.log("if is working");
       }
-      // toast.success("increase product", {
-      //   position: toast.POSITION.BOTTOM_LEFT,
-      // });
+      toast.success(' increace product in a card', {
+        position: toast.POSITION.BOTTOM_LEFT
+    });
     },
     decrease: (state, action) => {
       const index = state.cart.findIndex((item) => item.id === action.payload);
-
       if (state.cart[index].CardQuantity === 1) {
         const deleteitem = (state.cart = state.cart.filter(
           (x) => x.id !== action.payload
         ));
         state.cart = deleteitem;
-        // toast.error("remove product from  a cart ", {
-        //   position: toast.POSITION.BOTTOM_LEFT,
-        // });
+        
       } else {
         if (state.cart[index].CardQuantity >= 1) {
           state.cart[index].CardQuantity -= 1;
-          console.log("if is working");
-          // toast.error("decrease product from card ", {
-          //   position: toast.POSITION.BOTTOM_LEFT,
-          // });
+         
         }
       }
-     
+      toast.error('remove product from card', {
+        position: toast.POSITION.BOTTOM_LEFT
+    });
     },
-    clearCart: (state, action) => {
+    clearCart: (state, ) => {
       state.cart = [];
-     
+      toast.error('card is clear', {
+        position: toast.POSITION.BOTTOM_LEFT
+    });
     },
     getTotal(state,action){
-      state.cart.reduce((cardtotal,cart)=>{
-const {price,quantity}=cart
-const totalitem=price*quantity
-cardtotal.total+=totalitem
-      },{
-        total:0,
-        quantity:0
-      })
+ let {total,quantity}= state.cart.reduce(
+  (cardTotal,cardItem)=>{
+    const {price,cartQuantity}=cardItem
+const totalItem=price*cartQuantity
+cardTotal.total += totalItem
+cardTotal.quantity += cartQuantity
+return cardTotal
+  },{
+    total:0,
+    quantity:0
+  }
+ )
+ state.cartTotalAmount=total
+ state.cartTotalQuantity=quantity
     }
   },
 });
-export const { increase, decrease, addToCart, clearCart, removeItem } =
+export const { increase, decrease, addToCart, clearCart, removeItem,getTotal } =
   CartReducer.actions;
 export default CartReducer.reducer;
