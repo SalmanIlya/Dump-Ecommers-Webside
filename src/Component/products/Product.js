985020c0cc9403c168663788c8b5839436712444
Salamanilyas <95139252+SalmanIlya@users.noreply.github.com>
2022-11-15 12:09:22 +0500

    add-pagnation-but-small-error

--- src/Component/products/Product.js
+++ src/Component/products/Product.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Rating from "../Ratings/Ratings";
 import "./style.css";
@@ -7,18 +7,24 @@ import { addToCart } from "../../Store/AddCart";
 
 const Product = (props) => {
   const { allProduct, category } = props;
+  const [a,b]=useState(0)
+  const [c,d]=useState(8)
   const dispatch = useDispatch();
 
   const products = category
     ? allProduct.filter((item) => item.category === category)
     : allProduct;
 
+
   return (
     <div className="w-100 ">
       <h3 className="text-center m-5"> Our Product</h3>
+     
       <div className="products container ">
-        {products.map((item) => {
-          const { id, image, categort, title, price, rating, count } = item;
+        {products.slice(a,c).map((item) => {
+          const { id, image, categort, title, price, rating,  } = item;
+          const {rate,count}=rating
+         const newitem=rate.toFixed()
           return (
             <div className="card " key={id}>
               <Link to={`/product/${id}`}>
@@ -33,7 +39,7 @@ const Product = (props) => {
                 <h5 className="card-title">
                   <strong>${price}</strong>{" "}
                   <Rating
-                    rating={rating}
+                    newitem={newitem}
                     numReviews={count}
                     className="color"
                   />
@@ -51,6 +57,22 @@ const Product = (props) => {
             </div>
           );
         })}
+      </div>
+      
+      <div className="mt-5 d-flex justify-content-between ">
+        <button className="btn btn-warning" onClick={()=>{
+          if(a===0){
+            b(0)
+          }else{
+          b(a-4)
+           d(c-4)
+          }
+           }}>{"<"}pervious</button>
+            <button className="btn btn-warning" onClick={()=>{
+          b(a+4)
+          d(c+4)
+          }
+           }>Next{">"}</button>
       </div>
     </div>
   );
